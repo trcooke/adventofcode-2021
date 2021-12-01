d123b9887699f318820be872944521657a8ff768
Tim Cooke <tim@trcooke.co.uk>
2021-12-01 07:33:24 +0000

    Day 1 done

--- src/main/java/com/timdrivendev/aoc2021/Day01.java
+++ src/main/java/com/timdrivendev/aoc2021/Day01.java
@@ -5,21 +5,47 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day01 {
 
     int part1() throws IOException {
         BufferedReader reader = getInput("InputDay01");
+        int countIncreased = 0;
+        int lastDepth = -1;
         for (String line; (line = reader.readLine()) != null;) {
+            int depth = Integer.parseInt(line);
+            if (lastDepth == -1) {
+                lastDepth = depth;
+            }
+            if (depth > lastDepth) {
+                countIncreased++;
+            }
+            lastDepth = depth;
         }
-        return 0;
+        return countIncreased;
     }
 
     int part2() throws IOException {
         BufferedReader reader = getInput("InputDay01");
+        int countIncreased = 0;
+        List<Integer> depths = new ArrayList<>();
         for (String line; (line = reader.readLine()) != null;) {
+            depths.add(Integer.parseInt(line));
         }
-        return 0;
+        Integer lastAveDepth = -1;
+        for (int i = 0; i < depths.size() - 2; i++) {
+            Integer aveDepth = depths.get(i) + depths.get(i + 1) + depths.get(i + 2);
+            if (lastAveDepth == -1) {
+                lastAveDepth = aveDepth;
+            }
+            if (aveDepth > lastAveDepth) {
+                countIncreased++;
+            }
+            lastAveDepth = aveDepth;
+        }
+        return countIncreased;
     }
 
     private BufferedReader getInput(String inputFile) {
